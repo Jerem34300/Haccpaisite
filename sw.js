63fe15ffe76ca8e35b4d49af283fe472f36aa4b2
Jerem34300 <129687557+Jerem34300@users.noreply.github.com>
2026-09-20 23:24:06 +0200

    chore: bump SW cache haccpro-v393 -> v394 (dashboard photos fix)

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@
  * même si le réseau tablette est instable.
  */
 
-const CACHE_NAME = 'haccpro-v393';
-const CDN_CACHE_NAME = 'haccpro-cdn-v393';
+const CACHE_NAME = 'haccpro-v394';
+const CDN_CACHE_NAME = 'haccpro-cdn-v394';
 
 // Assets à mettre en cache dès l'installation
 // ⚠ NE PAS pré-cacher les pages HTML : elles utilisent Network-First
