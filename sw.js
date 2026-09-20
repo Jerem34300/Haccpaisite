c47a814ff053d730e312459c5ddc2f8e9f02ed7b
Jerem34300 <129687557+Jerem34300@users.noreply.github.com>
2026-09-21 00:42:02 +0200

    chore: bump SW cache to haccpro-v398 (ENR31/NC UX micro-fixes)

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@
  * même si le réseau tablette est instable.
  */
 
-const CACHE_NAME = 'haccpro-v397';
-const CDN_CACHE_NAME = 'haccpro-cdn-v397';
+const CACHE_NAME = 'haccpro-v398';
+const CDN_CACHE_NAME = 'haccpro-cdn-v398';
 
 // Assets à mettre en cache dès l'installation
 // ⚠ NE PAS pré-cacher les pages HTML : elles utilisent Network-First
